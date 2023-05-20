ac7c4e2a19b60f5ec5054cb18263c0686f182534
splendormagic <splendor.magic@gmail.com>
2023-05-20 14:17:04 +0300

    no unity

---
+++ library/src/main/java/com/solodroid/ads/sdk/util/Constant.java
@@ -0,0 +1,20 @@
+package com.solodroid.ads.sdk.util;
+
+public class Constant {
+
+    public static final String AD_STATUS_ON = "1";
+    public static final String ADMOB = "admob";
+    public static final String STARTAPP = "startapp";
+
+    public static final String APPLOVIN = "applovin";
+    public static final String MOPUB = "mopub";
+
+    //startapp native ad image parameters
+    public static final int STARTAPP_IMAGE_XSMALL = 1; //for image size 100px X 100px
+    public static final int STARTAPP_IMAGE_SMALL = 2; //for image size 150px X 150px
+    public static final int STARTAPP_IMAGE_MEDIUM = 3; //for image size 340px X 340px
+    public static final int STARTAPP_IMAGE_LARGE = 4; //for image size 1200px X 628px
+
+
+
+}
